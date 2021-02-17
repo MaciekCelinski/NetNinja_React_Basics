f7896b93127a0b1f57b0f513617dffd8098bee8e
Maciek C <mac.celinski@gmail.com>
2021-02-17 09:45:26 +0100

    Lecture 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+	const handleClick = (e) => {
+		console.log("e: ", e);
+		console.log("hello ninjas");
+	};
+
+	const handleClickAgain = (name, e) => {
+		console.log("Hello", name, e.target.innerHTML);
+	};
+
 	return (
 		<div className="home">
 			<h2>Homepage</h2>
+			<button onClick={handleClick}>click me</button>
+			<button onClick={(e) => handleClickAgain("Maciek", e)}>
+				click me again
+			</button>
 		</div>
 	);
 };
