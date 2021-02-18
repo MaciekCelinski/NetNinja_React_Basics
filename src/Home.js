1cb8260dfe6bee807200ff27286bc0b7a4a1d85c
Maciek C <mac.celinski@gmail.com>
2021-02-18 12:36:22 +0100

    Lecture 18

--- src/Home.js
+++ src/Home.js
@@ -5,45 +5,18 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 	// state
-	const [blogs, setBlogs] = useState([
-		{
-			title: "My new website",
-			body: "Lorem ipsum ...",
-			author: "mario",
-			id: 1,
-		},
-		{ title: "Welcome Party", body: "Lorem ipsum ...", author: "yoshi", id: 2 },
-		{
-			title: "Web dev top tips",
-			body: "Lorem ipsum ...",
-			author: "mario",
-			id: 3,
-		},
-	]);
-
-	const [name, setName] = useState("mario");
+	const [blogs, setBlogs] = useState(null);
 
 	// functions/actions
-	const handleDelete = (id) => {
-		const newBlogsArray = blogs.filter((blog) => blog.id !== id);
-		setBlogs(newBlogsArray);
-	};
-
 	useEffect(() => {
-		console.log("Use effect run");
-	}, [blogs, name]);
+		fetch("http://localhost:8000/blogs")
+			.then((response) => response.json())
+			.then((data) => setBlogs(data));
+	}, []);
 
 	return (
 		<div className="home">
-			<BlogList blogs={blogs} title="Blogs list" handleDelete={handleDelete} />
-			<button
-				onClick={() => {
-					setName("Luigi");
-				}}
-			>
-				Change Name
-			</button>
-			<h4>{name}</h4>
+			{blogs && <BlogList blogs={blogs} title="Blogs list" />}
 		</div>
 	);
 };
