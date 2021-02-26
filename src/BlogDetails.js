c1a0909eabebc0f31b155ab4d959d25d84ff22d2
Maciek C <mac.celinski@gmail.com>
2021-02-26 09:00:34 +0100

    Lecture 31

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,14 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 // custom hook
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 	const { id } = useParams();
 
+	// history hook
+	const history = useHistory();
+
 	// custom hook
 	const { data: blog, isPending, error } = useFetch(
 		"http://localhost:8000/blogs/" + id
 	);
+
+	const deleteBlog = () => {
+		fetch(`http://localhost:8000/blogs/${id}`, {
+			method: "DELETE",
+		}).then(() => {
+			history.push("/");
+		});
+	};
 
 	return (
 		<div className="blog-details">
@@ -19,6 +30,7 @@ const BlogDetails = () => {
 					<h2>{blog.title}</h2>
 					<p>Written by: {blog.author}</p>
 					<div>{blog.body}</div>
+					<button onClick={deleteBlog}>Delete</button>
 				</article>
 			)}
 		</div>
